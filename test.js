2224eac0774bc88809a00643d4e8533c2bfcfd75
Svetlozar Argirov <zarrro@gmail.com>
2017-11-08 17:08:58 +0200

    Linux support , WIP

--- test.js
+++ test.js
@@ -1,37 +1,5 @@
 const xmp  = require('./');
 
-const rdf=`
-  <rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#">
-    <rdf:Description rdf:about="" xmlns:dc="http://purl.org/dc/elements/1.1/">
-                  <dc:title>
-                     <rdf:Alt>
-                       <rdf:li xml:lang="x-default">An English Title</rdf:li>
-                       <rdf:li xml:lang="en-US">An English Title</rdf:li>
-                       <rdf:li xml:lang="fr-FR">Un Titre Francais</rdf:li>
-                     </rdf:Alt>
-                  </dc:title>
-                  <dc:description>
-                   <rdf:Alt>
-                     <rdf:li xml:lang="x-default">Green Bush</rdf:li>
-                   </rdf:Alt>
-                  </dc:description>
-                  <dc:format>image/jpeg</dc:format>
-                  <dc:creator>
-                   <rdf:Seq>
-                     <rdf:li>Author Name</rdf:li>
-                   </rdf:Seq>
-                  </dc:creator>
-    </rdf:Description>
-  </rdf:RDF>
-`
-
-// m = xmp.XMPMeta(rdf)
-// console.log(m.Serialize());
-// console.log(m.Properties());
-// for(let v in xmp.iteratorOptions){
-//   console.log(v);
-//   console.log(m.Properties(xmp.iteratorOptions[v]));
-// }
 filename = 'XMPSpecificationPart1.pdf'
 f = xmp.XMPFile()
 //console.log('IsMetadataWritable=', f.IsMetadataWritable(filename))
@@ -51,10 +19,11 @@ for(let prop of props){
     }
 }
 
-// em = xmp.XMPMeta()
-// console.log('CanPutXMP=', f.CanPutXMP(em));
-// f.PutXMP(em)
-// f.CloseFile()
+em = xmp.XMPMeta()
+em.DeleteProperty()
+console.log('CanPutXMP=', f.CanPutXMP(em));
+f.PutXMP(em)
+f.CloseFile()
 
 // for(let v in xmp.iteratorOptions){
 //   console.log(v);
