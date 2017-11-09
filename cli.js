e48034a49d9ebf66c5c0b81119dc403d01d02036
Svetlozar Argirov <zarrro@gmail.com>
2017-11-09 17:11:13 +0200

    ...

---
+++ cli.js
@@ -0,0 +1,24 @@
+const xmp = require('./');
+const commandLineArgs = require('command-line-args')
+
+const optionDefinitions = [
+  {name:'files', multiple: true, defaultOption: true}
+]
+
+const options = commandLineArgs(optionDefinitions);
+console.log(options);
+for(const filename of options.files) {
+  f = xmp.XMPFile()
+  f.OpenFile(filename, xmp.fileFlags.OpenForUpdate)
+  m = f.GetXMP()
+
+  props = m.ListProperties()
+  for(let prop of props){
+      let v = m.GetProperty(prop.ns, prop.path);
+      console.log(prop.path, '=',v);
+  }
+  // em = xmp.XMPMeta()
+  // console.log('CanPutXMP=', f.CanPutXMP(em));
+  // f.PutXMP(em)
+  // f.CloseFile()
+}
